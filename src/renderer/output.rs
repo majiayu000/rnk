569071b0b0bcbb920c5ae6fbc79946a67e853447
majiayu000 <1835304752@qq.com>
2026-02-08 14:10:02 +0800

    refactor(renderer): remove unused output cell mut accessor

--- src/renderer/output.rs
+++ src/renderer/output.rs
@@ -124,17 +124,6 @@ impl Output {
         }
     }
 
-    /// Get a mutable reference to a cell at (col, row)
-    #[inline]
-    fn get_mut(&mut self, col: usize, row: usize) -> Option<&mut StyledChar> {
-        if col < self.width as usize && row < self.height as usize {
-            let idx = self.index(col, row);
-            Some(&mut self.grid[idx])
-        } else {
-            None
-        }
-    }
-
     /// Set a cell at (col, row)
     #[inline]
     fn set(&mut self, col: usize, row: usize, value: StyledChar) {
